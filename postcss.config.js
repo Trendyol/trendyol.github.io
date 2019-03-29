1e8c82bafbe151437d62a4f70eba1222052bbb0b
Ogün Babacan <ogun.babacan@trendyol.com>
2019-03-29 09:43:58 +0300

    gh-page

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => {
-  const plugins = env === 'production'
-    ? [require('autoprefixer'), require('cssnano')]
-    : [require('autoprefixer')];
-  return {
-    plugins,
-    map: false,
-  };
-};
